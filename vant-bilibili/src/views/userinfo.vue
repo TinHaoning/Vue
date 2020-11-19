807c78aa7202d2adc6a5559403c3282b483ea61e
tianhaoning <tianhaoning@gangwantech.com>
2020-11-19 12:02:37 +0800

    个人中心页面绘制

--- vant-bilibili/src/views/userinfo.vue
+++ vant-bilibili/src/views/userinfo.vue
@@ -1,23 +1,30 @@
 <template>
-    <div>
-        <nav-bar></nav-bar>
-    </div>
+  <div class="userinfo">
+    <nav-bar />
+    <img src="@/assets/bannerTop_new.png" class="backimg" />
+    <user-detail/>
+  </div>
 </template>
 
 <script>
-import NavBar from '@/components/common/Navbar.vue'
+import NavBar from "@/components/common/Navbar.vue";
+import userDetail from "@/components/userComponent/userDetail.vue"
 export default {
-    components: {
-        NavBar,
-    },
-    data() {
-        return {
-
-        };
-    }
+  components: {
+    NavBar,
+    userDetail,
+  },
+  data() {
+    return {};
+  },
 };
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+.userinfo {
+  .backimg {
+    height: 131px;
+    width: 100%;
+  }
+}
 </style>
