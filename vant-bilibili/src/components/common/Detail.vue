131d27b6a790da36d1c50f68916145610fa6b796
tianhaoning <tinlink@yeah.net>
2020-11-26 10:47:30 +0800

    首页获取数据展示

--- vant-bilibili/src/components/common/Detail.vue
+++ vant-bilibili/src/components/common/Detail.vue
@@ -1,24 +1,24 @@
 <template>
-  <div>
-      <div class="detailItem">
-          <img :src="detailitem.img" alt="" style="width:100%">
-          <p>{{detailitem.name}}</p>
-      </div>
-  </div>
+    <div>
+        <div class="detailItem">
+            <img :src="detailitem.img" alt="" style="width:100%">
+            <p>{{detailitem.name}}</p>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  props: ["detailitem"],
-  data() {
-    return {};
-  },
+    props: ["detailitem"],
+    data() {
+        return {};
+    },
 };
 </script>
 
 <style lang="less" scoped>
-.detailItem{
-    p{
+.detailItem {
+    p {
         font-size: 3.2vw;
     }
 }
