db3d6f6f1d8b1ce75ef016a84b71a492476f9420
tianhaoning <tianhaoning@gangwantech.com>
2020-12-14 15:28:08 +0800

    修改导航栏bug

--- vant-bilibili/src/components/common/Navbar.vue
+++ vant-bilibili/src/components/common/Navbar.vue
@@ -1,108 +1,94 @@
 <template>
-  <div class="navbar">
-    <div class="logo">
-      <img src="@/assets/logo.png" @click="$router.push('/')" />
-    </div>
-    <div>
-      <p>
-        请输入内容搜索
-        <van-icon class="ipt-icon" name="search" />
-      </p>
-    </div>
-    <div>
-      <img
-        :src="imgUrl"
-        alt=""
-        @click="$router.push('/userinfo')"
-        v-if="imgUrl"
-      />
-      <img
-        src="@/assets/default_img.jpg"
-        alt=""
-        @click="$router.push('/login')"
-        v-else
-      />
-      <p>下载app</p>
-    </div>
+  <div class="NavBar">
+      <div>
+          <img src="@/assets/logo.png" alt="" @click="$router.push('/')">
+      </div>
+      <div>
+          <div> <van-icon name="search" class="iconNav"/></div>
+      </div>
+      <div>
+          <img :src="imgUrl" alt="" @click="$router.push('/userinfo')" v-if="imgUrl">
+          <img  src="@/assets/default_img.jpg" alt="" @click="$router.push('/login')"  v-else>
+          <div>下载App</div>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      imgUrl: "",
-    };
-  },
-  methods: {
-    async NavInit() {
-      if (localStorage.getItem("token")) {
-        const res = await this.$http.get("/user/" + localStorage.getItem("id"));
-        // console.log(res);
-        this.imgUrl = res.data[0].user_img;
-      }
+    data() {
+        return {
+            imgUrl:''
+        }
     },
-  },
-  mounted() {
-    this.NavInit();
-  },
-};
+    methods:{
+        async NavInit() {
+            if(localStorage.getItem('token')){
+            const res = await this.$http.get('/user/' + localStorage.getItem('id'))
+            this.imgUrl = res.data[0].user_img
+            }
+        }
+    },
+    mounted() {
+        this.NavInit()
+    }
+}
 </script>
 
-<style lang="less" scoped>
-.navbar {
-  height: 12vw;
-  background-color: #fff;
-  display: flex;
-  .logo {
+<style scoped lang="less">
+.NavBar{
+    height: 13.056vw;
     display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 24vw;
-    img {
-      width: 100%;
+    background-color: white;
+    div:nth-child(1){
+        width: 25vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        img{
+            width:100%;
+        }
     }
-  }
-  div:nth-child(2) {
-    flex: 1;
-    display: flex;
-    align-items: center;
-    margin: 0 1.333vw;
-    p {
-      font-size: 3.2vw;
-      line-height: 6.933vw;
-      background-color: #f4f4f4;
-      height: 6.133vw;
-      position: relative;
-      padding-left: 5.333vw;
-      width: 100%;
-      border-radius: 3.467vw;
-      color: #aaa;
-      .ipt-icon {
-        position: absolute;
-        left: 2.667vw;
-        top: 50%;
-        transform: translate(0, -40%);
-        color: #aaa;
-      }
+    div:nth-child(2){
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0 1.389vw;
+        div{
+            background-color: #f4f4f4;
+            height: 6.667vw;
+            width: 100%;
+            border-radius: 3.333vw;
+              position: relative;
+              .iconNav{
+                  color: #aaa;
+                  position: absolute;
+                  left: 2.778vw;
+                  top: 50%;
+                  transform: translate(0,-50%);
+            }
+     }
     }
-  }
-  div:nth-child(3) {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    img {
-      width: 6.4vw;
-      height: 6.4vw;
+    div:nth-child(3){
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      
+        >div{
+            margin: 0 2.778vw;
+            font-size: 3.611vw;
+            background-color: #fb7299;
+            padding: 1.389vw 2.778vw;
+            text-align: center;
+            border-radius: 0.833vw;
+            color: white;
+            
+        }
+        img{
+            width: 6.944vw;
+            height: 6.944vw;
+        }
     }
-    p {
-      padding: 0 2.667vw;
-      margin: 0 2.133vw;
-      background-color: #fb7299;
-      color: #fff;
-      font-size: 3.467vw;
-      border-radius: 0.8vw;
-    }
-  }
 }
 </style>
