5f82912996917cb2581a072abd5438558de3b254
tianhaoning <tinlink@yeah.net>
2020-12-15 10:52:47 +0800

    二级评论功能

--- src/views/cover.vue
+++ src/views/cover.vue
@@ -3,10 +3,10 @@
       <div class="detailItem">
           <div class="imgparent">
                <img :src="detailitem.img"  alt="" style="width:100%;height:27.778vw;">
-              <!-- <div class="bottom">
+              <div class="bottom">
                   <div class="icon-play2"><span class="video">1233</span></div>
                   <div class="icon-file-text"> <span class="comment">{{!detailitem.commentlen ? 66 : detailitem.commentlen}}</span> </div>
-              </div> -->
+              </div>
           </div>
           <p>{{detailitem.name}}</p>
       </div>
