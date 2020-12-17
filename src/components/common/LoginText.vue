3dccf5ab8d8df0a5f4bf0c6d09a34f7e815adf9c
Tianhaoning <Tinlink@yeah.net>
2020-12-17 20:27:27 +0800

    登录按钮

---
+++ src/components/common/LoginText.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+    <van-field
+    :label="label"
+    :placeholder="placeholder"
+    :type="type"
+    :rule="rule"
+    v-model="content"
+     />
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    data() {
+        return {
+            content:'',
+        }
+    },
+    props:['label','placeholder','type','rule'],
+    methods:{
+        handlerulg() {
+            const rue = new RegExp(this.rule)
+            // console.log(rue); 
+            if(rue.test(this.content)) {
+                // console.log('正则校验成功');
+                this.$emit('inputChange',this.content)
+            }
+        }
+    },
+    watch:{
+        content(){
+            this.handlerulg()
+            // console.log('监听成功');
+        }
+    }
+
+  }
+</script>
+
+<style scoped lang='scss'>
+
+</style>
